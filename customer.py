9638ad0085341567e9ec66787d0791a28723d436
MunafHaiderSezan <sezan22205101115@diu.edu.bd>
2024-11-13 00:09:04 +0600

    Add files via upload

---
+++ customer.py
@@ -0,0 +1,232 @@
+from tkinter import*
+from PIL import Image, ImageTk  #pip install pillow
+from tkinter import ttk
+
+       
+class Cust_Win:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Hotel Management System")
+        self.root.geometry("1295x580+230+220")
+
+
+         # ==================================== title ============================================================
+        lbl_title = Label(self.root, text="ADD CUSTOMER DETAILS", font=("times new roman", 18, "bold"), bg="black", fg="gold", bd=4, relief=Tk.RIDGE)
+        lbl_title.place(x=0, y=0, width=1295, height=50)
+
+        # ===================================== logo ===========================================================
+        img2 = Image.open(r"C:\Users\munaf\Pictures\Hotel Management System\Hotel Management System\1633410403702hotel-images\hotel images\logohotel1.png")
+        img2 = img2.resize((100, 40), Image.LANCZOS)
+        self.photoimg2 = ImageTk.PhotoImage(img2)
+
+        lbl_logo = tk.Label(self.root, image=self.photoimg2, bd=0, relief=tk.RIDGE)
+        lbl_logo.place(x=5, y=2, width=100, height=40)
+
+
+        #========================================labelFrame============================================================
+        labelframeleft=LabelFrame(self.root,bd=2,relief=RIDGE,text="Customer Details",font=("times new roman", 12, "bold"),padx=2,)
+        labelframeleft.place(x=5,y=50,width=425,height=490)
+
+        #=========================================labels and entrys=====================================================
+        #custRef
+        lbl_cust_ref=Label(labelframeleft,text="Customer Ref",font=("times new roman", 18, "bold"),padx=2,pady=6)
+        lbl_cust_ref.grid(row=0,column=0,sticky=W)
+
+        enty=ref=ttk.Entry(labelframeleft,width==29,font=("times new roman", 13, "bold"))
+        enty_ref.grid(row=0,column=1)
+
+        # cust name
+        cname=Label1(labelframeleft,font=("arial",12,"bold"),text="Customer Name:",padx=2,pady=6)
+        cname.grid(row=1,column=0,sticky=W)
+        txtcname=ttk.Entry(labelframeleft,font=("arial",13,"bold"),width=29)
+        txtcname.grid(row=1,column=1)
+
+        # mother name
+        lblmname=Label(labelframeleft,font=("arial",12,"bold"),text="Mother Name:",padx=2,pady=6)
+        lblmname.grid(row=2,column=0,sticky=W)
+        txtmname=ttk.Entry(labelframeleft,font=("arial",13,"bold"),width=29)
+        txtmname.grid(row=2,column=1)
+
+        # gender combox
+        label_gender=Label1(labelframeleft,font=("arial",12,"bold"),text="Gender:",padx=2,pady=6)
+        label_gender.grid(row=3,column=0,sticky=w)
+        combo_gender=ttk.Combobox(labelframeleft,)font=("arial",12,"bold"),width=27,state="readonly")
+        combo_gender["value"]=("Male","Female","Other")
+        combo_gender.current(0)
+        combo_gender.grid(row=3,column=1)
+
+
+
+        # postcode
+        lblPostCode=Label(labelframeleft,font=("arial",12,"bold"),text="PostCode:",padx=2,pady=6)
+        lblPostCode.grid(row=4,column=0,sticky=W)
+        txtPostCode=ttk.Entry(labelframeleft,font=("arial",13,"bold"),width=29)
+        txtPostCode.grid(row=4,column=1)
+
+        # mobilenumber
+        lblMobile=Label(labelframeleft,font=("arial",12,"bold"),text="Mobile:",padx=2,pady=6)
+        lblMobile.grid(row=5,column=0,sticky=W)
+        txtMobile=ttk.Entry(labelframeleft,font=("arial",13,"bold"),width=29)
+        txtMobile.grid(row=5,column=1)
+
+        # email
+        lblEmail=Label(labelframeleft,font=("arial",12,"bold"),text="Email:",padx=2,pady=6)
+        lblEmail.grid(row=6,column=0,sticky=W)
+        txtEmail=ttk.Entry(labelframeleft,font=("arial",13,"bold"),width=29)
+        txtEmail.grid(row=6,column=1)
+
+        # nationality
+        lblNationality=Label(labelframeleft,font=("arial",12,"bold"),text="Nationality:",padx=2,pady=6)
+        lblNationality.grid(row=7,column=0,sticky=W)
+        
+        combo_Nationality=ttk.Combobox(labelframeleft,)font=("arial",12,"bold"),width=27,state="readonly")
+        combo_Nationality["value"]=("Bangladeshi","Indian","American","British")
+        combo_Nationality.current(0)
+        combo_Nationality.grid(row=7,column=1)
+
+
+
+        # idproof type combox
+        lblIdProof=Label(labelframeleft,font=("arial",12,"bold"),text="Id Proof Type:",padx=2,pady=6)
+        lblIdProof.grid(row=8,column=0,sticky=W)
+
+        combo_id=ttk.Combobox(labelframeleft,)font=("arial",12,"bold"),width=27,state="readonly")
+        combo_id["value"]=("National ID CARD","Passport","Driving License","Birth Certificate")
+        combo_id.current(0)
+        combo_id.grid(row=8,column=1)
+
+
+
+
+        # id number
+        lblIdNumber=Label(labelframeleft,font=("arial",12,"bold"),text="Id Number:",padx=2,pady=6)
+        lblIdNumber.grid(row=8,column=0,sticky=W)
+        txtIdNumber=ttk.Entry(labelframeleft,font=("arial",13,"bold"),width=29)
+        txtIdNumber.grid(row=9,column=1)
+
+        # address
+        lblAddress=Label(labelframeleft,font=("arial",12,"bold"),text="Address:",padx=2,pady=6)
+        lblAddress.grid(row=8,column=0,sticky=W)
+        txtAddress=ttk.Entry(labelframeleft,font=("arial",13,"bold"),width=29)
+        txtAddress.grid(row=10,column=1)
+
+        #========================================btns============================================================
+        btn_frame=Frame(labelframeleft,bd=2,relief=RIDGE)
+        btn_frame.place(x=0,y=400,width=412,height=40)
+
+        btnAdd=Button(btn_frame,text="Add",font=("arial"12,"bold"),bg="black",fg="gold",width=10)
+        btnAdd.grid(row=0,column=0,padx=1)
+
+        btnUpdate=Button(btn_frame,text="Update",font=("arial"12,"bold"),bg="black",fg="gold",width=10)
+        btnUpdate.grid(row=0,column=1,padx=1)
+
+        btnDelete=Button(btn_frame,text="Delete",font=("arial"12,"bold"),bg="black",fg="gold",width=10)
+        btnDelete.grid(row=0,column=2,padx=1)
+
+        btnReset=Button(btn_frame,text="Reset",font=("arial"12,"bold"),bg="black",fg="gold",width=10)
+        btnReset.grid(row=0,column=10,padx=1)
+
+        #==========================================table frame===================================================
+
+        Table_Frame=LabelFrame(self.root,bd=2,relief=RIDGE,text="View Details And Search System",font=("times new roman", 12, "bold"),padx=2,)
+        Table_Frame.place(x=435,y=50,width=860,height=490)
+
+        lblSearchBy=Label(Table_Frame,font=("arial",12,"bold"),text="Search By:",bg="red",fg="white")
+        lblSearchBy.grid(row=0,column=0,sticky=W,padx=2)
+
+        combo_Search=ttk.Combobox(Table_Frame,font=("arial",12,"bold"),width=24,state="readonly")
+        combo_Search["value"]=("Mobile","Passport","Reference")
+        combo_Search.current(0)
+        combo_Search.grid(row=0,column=1,padx=2)
+
+        txtSearch=ttk.Entry(Table_Frame,font=("arial",13,"bold"),width=24)
+        txtSearch.grid(row=0,column=2,padx=2)
+
+        btnSearch=Button(Table_Frame,text="Search",font=("arial"12,"bold"),bg="black",fg="gold",width=10)
+        btnSearch.grid(row=0,column=3,padx=1)
+
+        btnShowAll=Button(Table_Frame,text="Show All",font=("arial"12,"bold"),bg="black",fg="gold",width=10)
+        btnShowAll.grid(row=0,column=4,padx=1)
+
+        #=============================================Show Data Table===========================================
+
+        details_table=Frame(Table_Frame,bd=2,relief=RIDGE)
+        details_table.place(x=0,y=50,width=860,height=350)
+
+        scroll_x=ttk.Scrollbar(details_table,orient=HORIZONTAL)
+        scroll_y=ttk.Scrollbar(details_table,orient=VERTICAL)
+
+        self.Cust_Details_Table=ttk.Treeview(details_table,column=("ref","name","mother","gender","post","mobile","email","nationality","idproof","idnumber","address"),xscrollcommand=scroll_x.set,yscrollcommand=scroll_y.set)
+        
+
+        scroll_x.pack(side=BOTTOM,fill=X)
+        scroll_y.pack(side=RIGHT,fill=Y)
+
+        scroll_x.config(command=self.Cust_Details_Table.xview)
+        scroll_y.config(command=self.Cust_Details_Table.yview)
+
+        self.Cust_Details_Table.heading("ref",text="Refer No")
+        self.Cust_Details_Table.heading("name",text="Name")
+        self.Cust_Details_Table.heading("mother",text="Mother Name")
+        self.Cust_Details_Table.heading("gender",text="Gender")
+        self.Cust_Details_Table.heading("post",text="PostCode")
+        self.Cust_Details_Table.heading("mobile",text="Mobile")
+        self.Cust_Details_Table.heading("email",text="Email")
+        self.Cust_Details_Table.heading("nationality",text="Nationality")
+        self.Cust_Details_Table.heading("idproof",text="Id Proof")
+        self.Cust_Details_Table.heading("idnumber",text="Id Number")
+        self.Cust_Details_Table.heading("address",text="Address")
+
+        self.Cust_Details_Table["show"]="headings"
+
+        self.Cust_Details_Table.heading("ref",text="Refer No",width=100)
+        self.Cust_Details_Table.heading("name",text="Name",width=100)
+        self.Cust_Details_Table.heading("mother",text="Mother Name",width=100)
+        self.Cust_Details_Table.heading("gender",text="Gender",width=100)
+        self.Cust_Details_Table.heading("post",text="PostCode",width=100)
+        self.Cust_Details_Table.heading("mobile",text="Mobile",width=100)
+        self.Cust_Details_Table.heading("email",text="Email",width=100)
+        self.Cust_Details_Table.heading("nationality",text="Nationality",width=100)
+        self.Cust_Details_Table.heading("idproof",text="Id Proof",width=100)
+        self.Cust_Details_Table.heading("idnumber",text="Id Number",width=100)
+        self.Cust_Details_Table.heading("address",text="Address",width=100)
+
+        self.Cust_Details_Table.pack(fill=BOTH,expand=1)
+
+
+
+       
+
+
+
+
+
+    
+
+
+
+
+
+
+
+       
+
+
+
+
+
+        
+        
+
+        enty=ref=ttk.Entry(labelframeleft,width==29,font=("times new roman", 13, "bold"))
+        enty_ref.grid(row=0,column=1)
+
+
+
+
+
+
+if __name__ == "__main__":
+    root=Tk()
+    obj=Cust_Win(root)
+    root.mainlNationality
---
+++ hotel.py
@@ -0,0 +1,95 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+from customer import Cust_Win
+
+class HotelManagementSystem:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Hotel Management System")
+        self.root.geometry("1550x800+0+0")
+
+        # ==================================== 1st img ========================================================
+        img1 = Image.open(r"C:\Users\munaf\Pictures\Hotel Management System\Hotel Management System\1633410403702hotel-images\hotel images\hotel1.png")
+        img1 = img1.resize((1550, 140), Image.LANCZOS)
+        self.photoimg1 = ImageTk.PhotoImage(img1)
+
+        lblimg = tk.Label(self.root, image=self.photoimg1, bd=4, relief=tk.RIDGE)
+        lblimg.place(x=0, y=0, width=1550, height=140)
+
+        # ===================================== logo ===========================================================
+        img2 = Image.open(r"C:\Users\munaf\Pictures\Hotel Management System\Hotel Management System\1633410403702hotel-images\hotel images\logohotel1.png")
+        img2 = img2.resize((230, 140), Image.LANCZOS)
+        self.photoimg2 = ImageTk.PhotoImage(img2)
+
+        lbl_logo = tk.Label(self.root, image=self.photoimg2, bd=4, relief=tk.RIDGE)
+        lbl_logo.place(x=0, y=0, width=230, height=140)
+
+        # ==================================== title ============================================================
+        lbl_title = tk.Label(self.root, text="HOTEL MANAGEMENT SYSTEM", font=("times new roman", 40, "bold"), bg="black", fg="gold", bd=4, relief=tk.RIDGE)
+        lbl_title.place(x=0, y=140, width=1550, height=50)
+
+        # ==================================== main Frame ========================================================
+        main_frame = tk.Frame(self.root, bd=4, relief=tk.RIDGE)
+        main_frame.place(x=0, y=190, width=1550, height=620)
+
+        # ===================================== menu ==============================================================
+        lbl_menu = tk.Label(main_frame, text="MENU", font=("times new roman", 20, "bold"), bg="black", fg="gold", bd=4, relief=tk.RIDGE)
+        lbl_menu.place(x=0, y=0, width=230)
+
+        # ==================================== button ==========================================================
+        btn_frame = tk.Frame(main_frame, bd=4, relief=tk.RIDGE)
+        btn_frame.place(x=0, y=35, width=228, height=190)
+
+        cust_btn = tk.Button(btn_frame, text="CUSTOMER", command=self.cust_details, width=22, font=("times new roman", 14, "bold"), bg="black", fg="gold", bd=0, cursor="hand1")
+        cust_btn.grid(row=0, column=0)
+
+        room_btn = tk.Button(btn_frame, text="ROOM", width=22, font=("times new roman", 14, "bold"), bg="black", fg="gold", bd=0, cursor="hand1")
+        room_btn.grid(row=1, column=0)
+
+        details_btn = tk.Button(btn_frame, text="DETAILS", width=22, font=("times new roman", 14, "bold"), bg="black", fg="gold", bd=0, cursor="hand1")
+        details_btn.grid(row=2, column=0)
+
+        report_btn = tk.Button(btn_frame, text="REPORT", width=22, font=("times new roman", 14, "bold"), bg="black", fg="gold", bd=0, cursor="hand1")
+        report_btn.grid(row=3, column=0)
+
+        logout_btn = tk.Button(btn_frame, text="LOGOUT", width=22, font=("times new roman", 14, "bold"), bg="black", fg="gold", bd=0, cursor="hand1")
+        logout_btn.grid(row=4, column=0)
+
+        # ====================================== right side image =============================================
+        img3 = Image.open(
+            r"C:\Users\munaf\Pictures\Hotel Management System\Hotel Management System\1633410403702hotel-images\hotel images\slide3.jpg")
+        img3 = img3.resize((1310, 590), Image.LANCZOS)
+        self.photoimg3 = ImageTk.PhotoImage(img3)
+
+        lblimg3 = tk.Label(main_frame, image=self.photoimg3, bd=4, relief=tk.RIDGE)
+        lblimg3.place(x=230, y=0, width=1310, height=590)
+
+        # ======================================= down images ==================================================
+        img4 = Image.open(
+            r"C:\Users\munaf\Pictures\Hotel Management System\Hotel Management System\1633410403702hotel-images\hotel images\myh.jpg")
+        img4 = img4.resize((230, 210), Image.LANCZOS)
+        self.photoimg4 = ImageTk.PhotoImage(img4)
+
+        lblimg4 = tk.Label(main_frame, image=self.photoimg4, bd=4, relief=tk.RIDGE)
+        lblimg4.place(x=0, y=225, width=230, height=210)
+
+        img5 = Image.open(
+            r"C:\Users\munaf\Pictures\Hotel Management System\Hotel Management System\1633410403702hotel-images\hotel images\khana.jpg")
+        img5 = img5.resize((230, 190), Image.LANCZOS)
+        self.photoimg5 = ImageTk.PhotoImage(img5)
+
+        lblimg5 = tk.Label(main_frame, image=self.photoimg5, bd=4, relief=tk.RIDGE)
+        lblimg5.place(x=0, y=440, width=230, height=190)
+
+
+        def cust_details(self):
+            self.new_window=Toplevel(self.root)
+            self.app=Cust_Win(self.new_window)
+
+    # Main execution
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    obj = HotelManagementSystem(root)
+    root.mainloop()
